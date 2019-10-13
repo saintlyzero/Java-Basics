daf163c78076bf31959d334472d869eec51501d2
= <=>
2019-10-13 21:47:54 +0530

    fixed even odd multithreading

--- multi_threading/EvenOdd.java
+++ multi_threading/EvenOdd.java
@@ -6,12 +6,13 @@
   * MultiThreading
   */
 
- class MultiThreadingEvenOdd implements Runnable{
+  class MultiThreadingEvenOdd implements Runnable{
  
     private static boolean evenFlag = true;
-
+  
     public synchronized void run() {
         if (evenFlag == true) {
+            evenFlag = false;
             printEven();
         } else {
            printOdd();
@@ -21,15 +22,14 @@
     public void printEven() {
         long threadId = Thread.currentThread().getId();
         for (int i = 0; i <= 10; i += 2) {
-            System.out.println(i+" ~ Thread "+threadId);
+            System.out.println(i+"\t~ Thread_id "+threadId);
         }
-        evenFlag = false;
     }
 
     public  void printOdd() {
         long threadId = Thread.currentThread().getId();
         for (int i = 1; i <= 10; i += 2) {
-            System.out.println(i+" ~ Thread "+threadId);
+            System.out.println(i+"\t~ Thread_id "+threadId);
         }
         evenFlag = true;
     }
@@ -39,10 +39,10 @@ public class EvenOdd {
     public static void main(String[] args) {
         
         MultiThreadingEvenOdd multiThreading = new MultiThreadingEvenOdd();
-        Thread thread1 = new Thread(multiThreading);
-        Thread thread2 = new Thread(multiThreading);
-        thread1.start();
-        thread2.start();
+        Thread evenThread = new Thread(multiThreading);
+        Thread oddThread = new Thread(multiThreading);
+        evenThread.start();
+        oddThread.start();
     }
     
 }
